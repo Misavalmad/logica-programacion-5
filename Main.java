36079c4e7e078d595f0895052833fb9be993ae04
Misavalmad <misavalmad@gmail.com>
2026-02-14 16:49:17 -0600

    feature: add spanish-english dictionary entries.

--- Main.java
+++ Main.java
@@ -3,7 +3,28 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        // fill dictionary and quiz logic in later steps.
         Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("casa", "house");
+        dictionary.put("perro", "dog");
+        dictionary.put("gato", "cat");
+        dictionary.put("libro", "book");
+        dictionary.put("mesa", "table");
+        dictionary.put("silla", "chair");
+        dictionary.put("ventana", "window");
+        dictionary.put("puerta", "door");
+        dictionary.put("coche", "car");
+        dictionary.put("ciudad", "city");
+        dictionary.put("agua", "water");
+        dictionary.put("comida", "food");
+        dictionary.put("escuela", "school");
+        dictionary.put("trabajo", "work");
+        dictionary.put("familia", "family");
+        dictionary.put("amigo", "friend");
+        dictionary.put("tiempo", "time");
+        dictionary.put("dia", "day");
+        dictionary.put("noche", "night");
+        dictionary.put("camino", "road");
+        dictionary.put("mundo", "world");
+        dictionary.put("musica", "music");
     }
 }
